be5e6520d44006798a7e6d91ac3ab4de13fbee2a
D.Miwa <suconbu@gmail.com>
2022-08-24 00:04:26 +0900

    Make to support lopen-data for Toyohashi-shi

--- gomiget_aichi_toyohashi.py
+++ gomiget_aichi_toyohashi.py
@@ -1,31 +1,22 @@
 #!/usr/bin/env python3
 
 import sys
-from gomireader import GomiReader, PatternValuePair, GomiArticle, overrides
-
-class AichiToyohashiGomiReader(GomiReader):
-    @overrides(GomiReader)
-    def get_article_from_texts(self, texts):
-        article = None
-        if 5 <= len(texts):
-            article = GomiArticle(
-                name=texts[0],
-                category=texts[3],
-                note=texts[-1:][0]
-            )
-        return article
+from gomireader import GomiReader, PatternValuePair
 
 def main(argv):
-    reader = AichiToyohashiGomiReader()
+    reader = GomiReader()
     reader.municipality_id = "232017"
     reader.municipality_name = "愛知県豊橋市"
-    reader.datasource_url = "http://www2.city.toyohashi.aichi.jp/kankyo/dictionary/index.html"
-    reader.target_url_base = "http://www2.city.toyohashi.aichi.jp/kankyo/dictionary/search.cgi?mode=index&key="
-    reader.target_pages = [ "1", "2", "3", "4", "5", "6", "7", "8", "9" ]
+    reader.datasource_url = "https://www.city.toyohashi.lg.jp/36806.htm"
+    reader.target_url_base = ""
+    reader.target_pages = [ "https://www.city.toyohashi.lg.jp/secure/60355/3r-bunbetsu.csv" ]
     reader.datetime_selector = None
     reader.datetime_pattern = None
-    reader.article_row_selector = "form > table > tr > td > table > tr > td > table > tr"
-    reader.article_column_selector = "td"
+    reader.article_row_selector = None
+    reader.article_column_selector = None
+    reader.article_index_name = 1
+    reader.article_index_category = 2
+    reader.article_index_note = 3
     reader.category_to_category_id = {
         PatternValuePair("生ごみ", "foodscraps"),
         PatternValuePair("もやすごみ", "burnable"),
@@ -38,7 +29,7 @@ def main(argv):
         PatternValuePair("プラマークごみ", "plasticpackaging"),
         PatternValuePair("布類", "cloth"),
         PatternValuePair("古紙", "localcollection.paper"),
-        PatternValuePair("家電４品目", "legalrecycling"),
+        PatternValuePair("家電4品目", "legalrecycling"),
         PatternValuePair("―", "uncollectible"),
         PatternValuePair("パソコン", "uncollectible"),
         PatternValuePair("収集しません", "uncollectible")
@@ -57,7 +48,7 @@ def main(argv):
         "cloth": { "name": "布類", "note": "月曜日又は火曜日（８週に１回）／透明又は半透明の袋" },
         "hazardous": { "name": "危険ごみ", "note": "水曜日（４週に１回）／透明又は半透明の袋" },
         "oversized": { "name": "大きなごみ", "note": "戸別有料収集又は資源化センターへの自己搬入" },
-        "legalrecycling": { "name": "家電４品目" },
+        "legalrecycling": { "name": "家電4品目" },
         "uncollectible": { "name": "収集しません" }
     }
     print(reader.to_json())
